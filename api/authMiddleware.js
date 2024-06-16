9c6bdc65200bc06aaa51ba6e6a1eb3994e901ed8
materubag <112043427+materubag@users.noreply.github.com>
2024-06-16 16:55:02 -0500

    Add files via upload

---
+++ api/authMiddleware.js
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken';
+import { db } from './db.js';
+
+const JWT_SECRET = "jwtkeyyyyyy";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+
+  if (!token) {
+    console.log("No token provided");
+    return res.status(401).json("Not authenticated!");
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      console.log("Error verifying token:", err);
+      return res.status(403).json("Token is not valid!");
+    }
+    
+    req.user = user; // Cambiar userInfo a user
+    next();
+  });
+};
+
+export const checkIfMod = (req, res, next) => {
+  const userId = req.user.id; // Cambiar userInfo a user
+  
+  const sql = 'SELECT `mod` FROM users WHERE id = ?';
+  db.query(sql, [userId], (err, results) => {
+    if (err) {
+      console.log("Database error:", err);
+      return res.status(500).json(err);
+    }
+
+    if (results.length > 0 && results[0].mod === 1) {
+      next();
+    } else {
+      console.log("User is not a mod");
+      res.status(403).json("User is not a mod");
+    }
+  });
+};
--- api/index.js
+++ api/index.js
@@ -1,25 +1,61 @@
 import express from "express";
 import cors from "cors"; 
 import cookieParser from "cookie-parser";
-
 import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/users.js";
+import modderRoutes from "./routes/modders.js"
 import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import multer from "multer";
+import path from "path"; 
+import fs from "fs";
 
 const app = express();
-
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:5173', 
+    credentials: true, 
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.delete("/api/delete-image/:filename", (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join("../client/public/upload", filename);
+
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error("Error al eliminar la imagen:", err);
+      res.status(500).json({ error: "Error al eliminar la imagen" });
+    } else {
+      res.status(200).json({ message: "Imagen eliminada correctamente" });
+    }
+  });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/modders", modderRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes); 
 
 app.get("/test", (req, res) => {
-    res.json("Funciona Carajo");
+    res.json("Funciona ");
 });
 
 app.listen(8080, () => {
